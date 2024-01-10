3654678daca666a742f6e0440393737287dcd75c
Kris-Chil <Kristinemchilson@gmail.com>
2024-01-10 11:06:00 +0100

    time for prototype!

--- Koi Punch Test/Assets/Scripts/ScoreManager.cs
+++ Koi Punch Test/Assets/Scripts/ScoreManager.cs
@@ -7,21 +7,42 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public TMP_Text endScoresVal;
+    public TMP_Text aScoresVal;
+    public TMP_Text bScoresVal;
+    public TMP_Text cScoresVal;
+    public TMP_Text dScoresVal;
+    public TMP_Text totalScoresVal;
     
     public TMP_Text highScoreText;
     public GameObject newHighScore;
 
-    public int scores;
-    public int endScores;
-    public int growthRate;
+    public int scoresA;
+    public int scoresB;
+    public int scoresC;
+    public int scoresD;
+    //public int totalScore;
+    public int endScoresA;
+    public int endScoresB;
+    public int endScoresC;
+    public int endScoresD;
+    public int totalEndScores;
+    public int growthRate = 1;
     public int highScore;
 
     public bool gameOver;
 
+    private void Start()
+    {
+        InvokeRepeating("CalculateScore",0f,0.01f);
+    }
+
     public void Update()
     {
-        endScoresVal.text = endScores.ToString("0");
+        aScoresVal.text = endScoresA.ToString("0");
+        bScoresVal.text = endScoresB.ToString("0");
+        cScoresVal.text = endScoresC.ToString("0");
+        dScoresVal.text = endScoresD.ToString("0");
+        totalScoresVal.text = totalEndScores.ToString("0");
         highScoreText.text = highScore.ToString();
         
         if (Input.GetKeyDown("space"))
@@ -31,20 +52,38 @@ public class ScoreManager : MonoBehaviour
 
         if (gameOver == true)
         {
-            CalculateScore();
+            Debug.Log("Game Over");
+            //CalculateScore();
+            //InvokeRepeating("CalculateScore",0f,1f);
         }
     }
 
     public void CalculateScore()
     {
-        if (endScores != scores && scores > endScores)
+        Debug.Log("Calculating Score");
+        if (endScoresA != scoresA && scoresA > endScoresA)
         {
-            endScores += growthRate;
+            endScoresA += growthRate;
+            totalEndScores += growthRate;
         }
-
-        if (endScores > highScore)
+        else if (endScoresB != scoresB && scoresB > endScoresB)
         {
-            highScore = endScores;
+            endScoresB += growthRate;
+            totalEndScores += growthRate;
+        }
+        else if (endScoresC != scoresC && scoresC > endScoresC)
+        {
+            endScoresC += growthRate;
+            totalEndScores += growthRate;
+        }
+        else if (endScoresD != scoresD && scoresD > endScoresD)
+        {
+            endScoresD += growthRate;
+            totalEndScores += growthRate;
+        }
+        else if (totalEndScores > highScore)
+        {
+            //highScore = totalEndScores;
             newHighScore.SetActive(true);
         }
     }
